330e3c41b32c2fa14b17d7706eff0ab34bd946a8
Patrick Bower <patrickbower@Patricks-MacBook-Pro.local>
2017-02-27 14:05:55 +0000

    Updates

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import cards from './cards';
-import settings from './settings';
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({
-    cards, settings
-});
-
-export default rootReducer;
