a68ff95473c72e7d89cc0a2434934e23ab74cca7
Patrick Bower <patrickbower@Patricks-MacBook-Pro.local>
2017-03-02 11:41:16 +0000

    Timer complete
    Configured timer.js plugin.
    Add SVG elements.
    Preliminary styling added.
    Controls added.

--- src/componants/Clock.js
+++ src/componants/Clock.js
@@ -12,40 +12,78 @@ class Clock extends Component {
     constructor(props){
         super(props);
 
+        this.state = ({
+            position: 0,
+            isRunning: false
+        })
+
+        this.countdownLength = 20;
+        this.countdownSeconds = this.countdownLength * 60;
+        this.linelengthTick = 282.74 / this.countdownSeconds;
+        this.runningLength = 0;
+
         this.countdown = new Timer({
             tick    : 1,
-            ontick  : function(ms) { console.log(ms + ' milliseconds left') },
-            onstart : function() { console.log('timer started') },
-            onstop  : function() { console.log('timer stop') },
-            onpause : function() { console.log('timer set on pause') },
-            onend   : function() { console.log('timer ended normally') }
+            ontick  : () => this.tick(),
+            onstart : () => this.started(),
+            onstop  : () => this.stopped(),
+            onpause : () => this.paused(),
+            onend   : () => this.ended()
         });
+    }
 
-        this.countdownLength = 15 * 60;
+    // every 60 seconds
+    tick() {
+        this.runningLength += this.linelengthTick;
+        let newPos = Math.round(this.runningLength * 100) / 100;
+        this.setState({ position: newPos });
+
+        console.log(newPos);
+    }
+
+    started() {
+        this.setState({ isRunning: true });
+    }
+
+    paused() {
+        this.setState({ isRunning: false });
+    }
+
+    ended() {
+        alert('Times up bucco!');
     }
 
     startTimer(){
 
-        // https://github.com/husa/timer.js
-        this.countdown.start(this.countdownLength).on('end', function () {
-          alert('Pizza is ready, bon appetit!');
-        });
+        if (this.state.isRunning) {
+            this.countdown.start();
+        } else {
+            this.countdown.start(this.countdownSeconds);
+        }
     }
 
     pauseTimer() {
         this.countdown.pause();
     }
 
-    unpauseTimer() {
-        this.countdown.start();
-    }
-
     render(){
+        let strokeDashoffset = this.state.position;
+        let buttonType = this.state.isRunning ?
+            <button className="timer-btn" onClick={ this.pauseTimer.bind(this) }><b>||</b></button> :
+            <button className="timer-btn" onClick={ this.startTimer.bind(this) }>&#9658;</button> ;
+
         return(
             <div>
-                <button onClick={ this.startTimer.bind(this) }>startTimer</button>
-                <button onClick={ this.pauseTimer.bind(this) }>pauseTimer</button>
-                <button onClick={ this.unpauseTimer.bind(this) }>unpauseTimer</button>
+
+                <div className="timer-wrap d-flex justify-content-center">
+                    { buttonType }
+                    <svg className="timer" width="150" height="150" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                        <circle ref="timeline" id="timeline" r="45" cx="75" cy="75"
+                                style={{strokeDashoffset}}>
+                        </circle>
+                    </svg>
+                </div>
+
             </div>
         )
     }
