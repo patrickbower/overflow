d1804021f727e3e149731230285bf08ac8141a53
Patrick Bower <patrickbower@Patricks-MacBook-Pro.local>
2017-03-03 17:26:56 +0000

    icons in and working as expected

--- src/componants/Cardlist.js
+++ src/componants/Cardlist.js
@@ -48,7 +48,16 @@ class Cardlist extends Component {
         }
     }
 
+    // <input type="checkbox" className="app__card-check"
+    //     checked={ listData.state === 'complete' ? true : false }
+    //     onChange={ () => this.props.actions.checkItem(index, key) }
+    // />
+
     checkitem = (listData, index, key) => {
+
+        let crossIcon = listData.state === 'complete' ? <span className="icon-action icon-action--cross"></span> : '';
+        let tickIcon = listData.activeItem ? <span className="icon-action icon-action--tick"></span> : '';
+
         return (
             <div
                 key={listData.id}
@@ -62,10 +71,11 @@ class Cardlist extends Component {
                 <div className={`app__card-background ${listData.activeItem ? 'active' : ''}`}></div>
                 <div className="app__card-indicator"></div>
                 <label className="app__card-label">{listData.name}</label>
-                <input type="checkbox" className="app__card-check"
-                    checked={ listData.state === 'complete' ? true : false }
-                    onChange={ () => this.props.actions.checkItem(index, key) }
-                />
+                <a href="#"
+                    onClick={ () => this.props.actions.checkItem(index, key) }
+                    className="app__card-icon">
+                        { crossIcon } { tickIcon }
+                </a>
             </div>
         )
     }
