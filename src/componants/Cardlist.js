d0963274a11bf93f32170aee7f0384ffb76c60d3
Patrick Bower <bower.patrick@gmail.com>
2017-03-06 10:52:49 +0000

    Style ui (#10)
    * Basic page styling
    Card list styling started
    
    * active card interaction working as expected
    
    * update to item state
    
    * icons in and working as expected
    
    * Active list styling complete

--- src/componants/Cardlist.js
+++ src/componants/Cardlist.js
@@ -30,19 +30,52 @@ class Cardlist extends Component {
         this.props.actions.newCheckItem(data, this.cardKeyAddItem);
     }
 
+    itemState = (index, key, state) => {
+
+        if (state === 'complete') return false;
+
+        let activeItemObj = this.props.cards[key].activeItem;
+
+        // new item - either new or others have been unselected
+        if (typeof activeItemObj === 'undefined' || activeItemObj === false) {
+            this.props.actions.activeItem(index, key);
+        // deselect an active item
+        } else if (typeof activeItemObj === 'number' && activeItemObj === index) {
+            this.props.actions.deselectActiveItem(index, key);
+        // select differnt item - unselect one and select a differnt one
+        } else if (typeof activeItemObj === 'number') {
+            this.props.actions.changeActiveItem(index, key);
+        }
+    }
+
+    // <input type="checkbox" className="app__card-check"
+    //     checked={ listData.state === 'complete' ? true : false }
+    //     onChange={ () => this.props.actions.checkItem(index, key) }
+    // />
+
     checkitem = (listData, index, key) => {
+
+        let crossIcon = listData.state === 'complete' ? <span className="icon-action icon-action--cross"></span> : '';
+        let tickIcon = listData.activeItem ? <span className="icon-action icon-action--tick"></span> : '';
+
         return (
-            <div key={listData.id}>
-                <div className="form-check mb-0">
-                    <label className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input app__custom-control-input"
-                            checked={ listData.state === 'complete' ? true : false }
-                            onChange={ () => this.props.actions.checkItem(index, key) }
-                        />
-                        <span className="custom-control-indicator app__custom-control-indicator" />
-                        <span className="custom-control-description app__custom-control-description">{listData.name}</span>
-                      </label>
-                </div>
+            <div
+                key={listData.id}
+                onClick={ () => this.itemState(index, key, listData.state) }
+                className={`
+                        app__card-item
+                        ${listData.state === 'complete' ? 'complete' : ''}
+                        ${listData.activeItem ? 'active' : ''}
+                    `}
+            >
+                <div className={`app__card-background ${listData.activeItem ? 'active' : ''}`}></div>
+                <div className="app__card-indicator"></div>
+                <label className="app__card-label">{listData.name}</label>
+                <a href="#"
+                    onClick={ () => this.props.actions.checkItem(index, key) }
+                    className="app__card-icon">
+                        { crossIcon } { tickIcon }
+                </a>
             </div>
         )
     }
@@ -50,10 +83,15 @@ class Cardlist extends Component {
     cards = (cardsData) => {
         return Object.keys(cardsData).map(key => {
             return (
-                <li key={key} className="app__card p-3 mb-3">
+                <li key={key}
+                    className={`
+                            app__card p-3 mb-3
+                            ${typeof cardsData[key].activeItem === 'number' ? 'active' : ''}
+                        `}
+                    ref="card">
                     <form>
                         <fieldset>
-                            <legend className="h5 mb-3">{ cardsData[key].name }</legend>
+                            <legend className="h5 mb-4 text-center font-weight-bold">{ cardsData[key].name }</legend>
                             { cardsData[key].checklist.map((listData, index) => this.checkitem(listData, index, key)) }
                         </fieldset>
                     </form>
